94df35849421ea4900a0b1f0c7e297d9020a3a46
Willy Aguirre <marti1125@gmail.com>
2016-11-02 02:35:20 -0500

    se agrega la insercion a base de datos

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ http://datosabiertos.miraflores.gob.pe/dataviews/231084/veterinarias/
 https://pypi.python.org/pypi/wget/
 
 https://docs.python.org/2/library/csv.html
+
+### Instrucciones
+
+    $ pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python
 import os, time
 import wget
 import csv
+import MySQLdb
 from stat import * # ST_SIZE etc
 
 file_url  = 'http://datosabiertos.miraflores.gob.pe/rest/datastreams/220665/data.csv'
 file_name = wget.download(file_url)
+
+# Se abre la conexion a la base de datos
+db = MySQLdb.connect("localhost","root","","veterinaria")
+
+# prepara el objeto cursor()
+cursor = db.cursor()
 
 try:
     st = os.stat(file_name)
@@ -17,4 +25,20 @@ else:
 with open('veterinarias.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['CODIGO'], row['NOMBRE-O-RAZON-SOCIAL'], row['NOMBRE-DE-VIA'], row['NRO'], row['LETRA'], row['INTERIOR'], row['GIRO'], row['NOMBRE-COMERCIAL'], row['FEC-INICIO'])
+        print(row['CODIGO'], row['NOMBRE-O-RAZON-SOCIAL'], row['NOMBRE-DE-VIA'], row['NRO'], row['LETRA'], row['INTERIOR'], row['GIRO'], row['FEC-INICIO'])
+        # crear la  sentencia sql
+        sql = "insert into local(CODIGO, NOMBREORAZONSOCIAL, NOMBREDEVIA, NRO, LETRA, INTERIOR, GIRO, FECINICIO) \
+               values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % \
+               (row['CODIGO'], row['NOMBRE-O-RAZON-SOCIAL'], row['NOMBRE-DE-VIA'], row['NRO'], row['LETRA'], row['INTERIOR'], row['GIRO'], row['FEC-INICIO'])
+        try:
+           cursor.execute(sql)
+           db.commit()
+        except:
+           db.rollback()
+
+# se desconecta con el servidor
+db.close()
+
+# retira el archivo original *.csv
+if os.path.exists('veterinarias.csv'):
+    os.remove('veterinarias.csv')

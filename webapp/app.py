a6094d7aff16138710ed90b70c305100552694f9
Willy Aguirre <marti1125@gmail.com>
2016-11-03 12:11:40 -0500

    ahora muestrar todas las veterinarias en mapbox

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import render_template
 import MySQLdb
 import collections
 app = Flask(__name__)
@@ -26,6 +27,8 @@ def local():
            d['id'] = row[0]
            d['codigo'] = row[1]
            d['razonsocial'] = row[2]
+           d['direccion'] = row[3] + " " + row[4]
+           d['giro'] = row[7]
            d['latitud'] = row[10]
            d['longitud'] = row[11]
            rows.append(d)
@@ -36,8 +39,8 @@ def local():
     return jsonify(rows)
 
 @app.route("/")
-def hello():
-    return "Veterinarias"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
